3bd9686c11fe2c075ef7a652020ded4a91d61b55
Carlos Arenas <carlos.arenas@zemoga.com>
2017-05-22 09:17:27 -0700

    First commit.

---
+++ moccafe/moccafe/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  moccafe
+//
+//  Created by Carlos Arenas on 5/22/17.
+//  Copyright © 2017 moccafe. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
