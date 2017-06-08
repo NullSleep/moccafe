f29ffde0e18956049c906950acb6eecb7e515132
Valentina Henao <valen410@gmail.com>
2017-06-08 09:58:44 -0700

    API Implementation

--- moccafe/moccafe/APICall.swift
+++ moccafe/moccafe/APICall.swift
@@ -12,62 +12,19 @@ import SwiftyJSON
 
 class APICall: NSObject {
     
-    //GET Blog ID
-    func getID(email: String, password: String, firstName: String, lastName: String, checkBox: Bool, completionHandler: @escaping (JSON?, Error?) -> ()) {
-        
-        let url = "https:// /blogs/news_blog_id"
-        let parameters = ["email": email, "first_name": firstName, "last_name": lastName, "newsletter":"\(checkBox)", "password": password, "password_confirmation": password]
-        
-        Alamofire.request(url, method: .get, parameters: parameters, encoding: JSONEncoding.default, headers: nil).responseJSON{ response in
-            print(response)
-            switch response.result {
-            case .success(let value):
-                print("value get blog id \(value)")
-                let json = JSON(rawValue: value)
-                completionHandler(json, nil)
-                
-            case .failure(let error): print("Error")
-            completionHandler(nil, error)
-                return
-            }
-        }
-}
-    //GET Blog
-    func getBlog(id: String, completionHandler: @escaping (JSON?, Error?) -> ()) {
-        
-        let url = "https:// /blogs/#{blog_id}"
-        let parameters = ["id": id]
-        
-        Alamofire.request(url, method: .get, parameters: parameters, encoding: JSONEncoding.default, headers: nil).responseJSON{ response in
-            print(response)
-            switch response.result {
-            case .success(let value):
-                print("value get blog \(value)")
-                let json = JSON(rawValue: value)
-                completionHandler(json, nil)
-                
-            case .failure(let error): print("Error")
-            completionHandler(nil, error)
-                return
-            }
-        }
-    }
+    //MARK: View and Edit Profile
     
-    
-//GET Profile
     func getProfile(completionHandler: @escaping (JSON?, Error?) -> ()) {
         
         let url = "https://app.moccafeusa.com/api/v1/customers/profile"
         
         Alamofire.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil).responseJSON {
-            
             response in
             switch response.result {
             case .success(let value):
                 print("get profile result \(value)")
                 let json = JSON(rawValue: value)
                 completionHandler(json, nil)
-                
             case .failure(let error): print("Error")
             completionHandler(nil, error)
                 return
@@ -75,16 +32,12 @@ class APICall: NSObject {
         }
     }
 
-
-    //POST Profile
     func postProfile(json: JSON, completionHandler: @escaping (JSON?, Error?) -> ()) {
         
         let url = "https://app.moccafeusa.com/api/v1/customers/update_profile"
-     //   let json1 = json.dictionaryObject
-        
-        let params = ["customer": json.dictionaryObject]
-     //   let json = JSON("customer", parameters)
+        let params = ["customer": json.dictionaryObject ?? ["":""]]
 
+        
         Alamofire.request(url, method: .post, parameters: params, encoding: JSONEncoding.default, headers: nil).responseString { response in
             
             switch response.result {
@@ -93,56 +46,82 @@ class APICall: NSObject {
             completionHandler(json, nil)
             case .failure(let error): print("ERROR")
             completionHandler(nil, error)
-
                 
             }
-            print("Response String: \(response)")
-            
-            
-           // let response = String(data: data, encoding: String.Encoding )
-            print("response \(response)")
-    //        switch response.result {
-    //        case .success(let value):
-    //            print("value edit profile \(value)")
-    //            let json = JSON(rawValue: value)
-    //            completionHandler(json, nil)
-    //            
-    //        case .failure(let error): print("Error")
-    //        completionHandler(nil, error)
-    //            return
-    //        }
         }
     }
+
+    //MARK: Submit Questions and question type options
+
+    func getList(completionHandler: @escaping (JSON?, Error?) -> ()) {
+        
+        let url = "https://app.moccafeusa.com/api/v1/questions/support_options"
+        
+        Alamofire.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil).responseJSON{ response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(rawValue: value)
+                completionHandler(json, nil)
+                
+            case .failure(let error): print("Error")
+            completionHandler(nil, error)
+                return
+            }
+        }
+    }
+
+    func postQuestion(json: JSON, completionHandler: @escaping (JSON?, Error?) -> ()) {
+        
+        let url = "https://app.moccafeusa.com/api/v1/questions/post_question"
+        let params = json.dictionaryObject
+        
+        Alamofire.request(url, method: .post, parameters: params, encoding: JSONEncoding.default, headers: nil).responseJSON{ response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(rawValue: value)
+                completionHandler(json, nil)
+            case .failure(let error): print("Error")
+            completionHandler(nil, error)
+                return
+            }
+        }
+    }
+
+    //MARK: Retrieve Blog details and articles
+    
+    func getBlogDetails(completionHandler: @escaping (JSON?, Error?) -> ()) {
+        
+        let url = "https://app.moccafeusa.com/api/v1/blogs/details"
+        
+        Alamofire.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil).responseJSON { response in
+            print(response)
+            switch response.result {
+            case .success(let value):
+                let json = JSON(rawValue: value)
+                completionHandler(json, nil)
+            case .failure(let error): print("Error")
+                completionHandler(nil, error)
+                return
+            }
+        }
+    }
+    
+    func retrieveArticles(json: JSON, completionHandler: @escaping (JSON?, Error?) -> ()) {
+    
+    let url = "https://app.moccafeusa.com/api/v1/blogs/tree_articles"
+    let params = json.dictionaryObject
+
+        Alamofire.request(url, method: .post, parameters: params, encoding: JSONEncoding.default, headers: nil).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(rawValue: value)
+                completionHandler(json, nil)
+            case .failure(let error): print("Error")
+                completionHandler(nil, error)
+                return
+            }
+        }
+    }
+
 }
 
-
-//var hourly_pays_attributes = [[String: Any]]()
-//var loads_attributes = [[String: Any]]()
-//let payTypes = jsonToUpdateInterviewAPI.array?[5]["answer"].array ?? [JSON]()
-//for item in (payTypes) {
-//    if (item["pay"].string ?? "pay").lowercased().contains("hour") {
-//        let hourlyPay = ["name" : "\(item["title"].string ?? "")", "rate" : "\(item["fee"].string ?? "name")" ]
-//        hourly_pays_attributes.append(hourlyPay)
-//    } else {
-//        let loadType = ["title" : "\(item["title"].string ?? "")", "fee" : "\(item["fee"].string ?? "name")", "pay" : "\(item["pay"].string ?? "")"]
-//        loads_attributes.append(loadType)
-//    }
-//}
-
-//POST https://app.moccafeusa.com/api/v1/customers/update_profile
-//{
-//    "customer": {
-//        "name": "Pepito",
-//        "last_name": "Perez",
-//        "email": "micorreo@moccafeusa.com",
-//        "address": "Direccion uno",
-//        "shipping": "Direccion dos - tal vez diferente a la direccion 1",
-//        "phone": "00000 tel fijo",
-//        "mobile": "315 00000",
-//        "info": "Hola, soy Pepito y en este campo pongo informacion sobre mi mismo. Me gusta el cafe.",
-//        "city": "New York",
-//        "country_id": 1,
-//        "user_status_id": 1,
-//        "gender_id": 1
-//    }
-//}
--- moccafe/moccafe/ContactUsViewController.swift
+++ moccafe/moccafe/ContactUsViewController.swift
@@ -12,14 +12,13 @@ import SwiftyJSON
 class ContactUsViewController: UIViewController, UIGestureRecognizerDelegate {
 
     @IBOutlet var nameLabel: UILabel!
-    
     @IBOutlet var emailLabel: UILabel!
-    
     @IBOutlet var commentsLabel: UILabel!
-    
     @IBOutlet var commentsField: UITextView!
     
     var json : JSON = [:]
+    
+    let apiHandler = APICall()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,12 +31,58 @@ class ContactUsViewController: UIViewController, UIGestureRecognizerDelegate {
         tap.delegate = self
         view.addGestureRecognizer(tap)
         
-        // Do any additional setup after loading the view.
+        apiHandler.getList { json, error in
+            print("json response get list \(json)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func submitQuestion(_ sender: UIButton) {
+    
+        if checkFields() {
+            var json: JSON = [:]
+            var params = [String: Any]()
+        
+            params["name"] = nameLabel.text
+            params["email"] = emailLabel.text
+            params["phone"] = "987345"
+            params["title"] = "Pregunta para el programa"
+            params["content"] = commentsField.text
+            params["question_type_id"] = 1
+
+            json["question"] = JSON(params)
+                
+            apiHandler.postQuestion(json: json) {
+            json, error in
+                print("respomse post question\(json ?? "")")
+            }
+        } else {
+            let alertView = UIAlertController(title: "Error", message: "Required fields must be filled out before proceeding", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default) { Void in }
+            alertView.addAction(action)
+            self.present(alertView, animated: true, completion: nil)
+        }
+    }
+    
+    func checkFields() -> Bool {
+        guard (nameLabel.text?.isEmpty == false), (emailLabel.text?.isEmpty == false), (commentsField.text.isEmpty == false) else { return false }
+        return true
+    }
+    
+    func retrieveArticle() {
+        var json: JSON = [:]
+        
+        json["blog"] = ["pagina": 1]
+        
+        apiHandler.retrieveArticles(json: json) {
+            json, error in
+            print(json)
+        }
     }
     
 
